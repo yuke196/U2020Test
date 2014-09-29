68e0d37286020f681595ca70d6cb0e14e1405c4b
yuke <363564023@qq.com>
2014-09-29 17:31:49 +0800

    starting u2020

--- src/main/java/com/jakewharton/u2020/ui/MainActivity.java
+++ src/main/java/com/jakewharton/u2020/ui/MainActivity.java
@@ -15,8 +15,7 @@ public class MainActivity extends Activity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    U2020App app = U2020App.get(this);
-    app.inject(this);
+    U2020App.get(this).inject(this);//inject self
 
     container = appContainer.get(this);
 
